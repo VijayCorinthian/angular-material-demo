bf5398b79f8ce000cb796b36aaa561ecd59b6660
Vijay David <vijayk@citybond.co.uk>
2019-07-26 13:47:37 +0100

    test commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MockTravellers } from './mock-travellers';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  mockTravellers = MockTravellers;  
+  control = new FormControl('myControl');  
+  email = new FormControl('', [Validators.required, Validators.email]);
   title = 'material2demo';
 }
