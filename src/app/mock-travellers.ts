01060e7fd91d6d8e073cb38069771cb51b27b49e
Vijay David <vijayk@citybond.co.uk>
2019-07-26 15:19:56 +0100

    test commit of stage from dev to master

--- src/app/mock-travellers.ts
+++ src/app/mock-travellers.ts
@@ -1,8 +1,9 @@
 import { Traveller } from './traveller';
 
 export const MockTravellers: Traveller[]=[
-    {id: 1, name: "Joe"},
+    {id: 1, name: "Joe.."},
     {id: 3, name: "Jim"},
     {id: 4, name: "Harrison"},
-    {id: 5, name: "Dev1"}
+    {id: 5, name: "Dev111"},
+    {id: 6, name: "Vijay"}
 ]
