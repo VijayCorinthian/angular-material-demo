3b6e1305898a4a7cb8a5e2f7857608eb46dc0e64
Vijay David <vijayk@citybond.co.uk>
2019-07-26 17:12:59 +0100

    commiting dev 2 traveller

--- src/app/mock-travellers.ts
+++ src/app/mock-travellers.ts
@@ -8,5 +8,6 @@ export const MockTravellers: Traveller[]=[
     {id: 6, name: "Vijay"},
     {id: 7, name: "Dan"},
     {id: 8, name: "Mike"},
-    {id: 9, name: "Olie"}
+    {id: 9, name: "Olie"},
+    {id: 10, name: "Dev 2"}
 ]
